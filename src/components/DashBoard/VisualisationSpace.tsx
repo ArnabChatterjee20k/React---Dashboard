d09cdc3bd9e163a52db19967ef36339cb1c1cdf8
arnab <arnabchatterjee.ac.2@gmail.com>
2023-09-22 21:09:28 +0530

    feat -- added scheduling data

--- src/components/DashBoard/VisualisationSpace.tsx
+++ src/components/DashBoard/VisualisationSpace.tsx
@@ -3,6 +3,8 @@ import DataCards from "./DataCards";
 import MonthlyChartProps from "../../types/MonthlyChart";
 import ProductChart from "../Charts/ProductChart";
 import ProductChartProps from "../../types/ProductChartProps";
+import ScheduleProps from "../../types/Schedule";
+import Schedule from "../Schedule/Schedule";
 
 const chartsData: MonthlyChartProps = {
   chartData: {
@@ -33,6 +35,20 @@ const productChartData: ProductChartProps = {
   },
 };
 
+const schedule:ScheduleProps = {
+  todaysSchedule: [
+    {
+      time: "14.00-15.00",
+      location: "Sunset Road, Kuta, Bali ",
+      meetingTitle: "Meeting with suppliers from Kuta Bali",
+    },
+    {
+      time: "18.00-20.00",
+      location: "Central Jakarta",
+      meetingTitle: "Check operation at Giga Factory 1",
+    },
+  ],
+};
 export default function VisualisationSpace() {
   return (
     <div className="w-full flex flex-col gap-7">
@@ -40,7 +56,7 @@ export default function VisualisationSpace() {
       <DisplayMonthChart chartData={chartsData.chartData} />
       <div className="flex flex-col md:flex-row gap-5 w-full md:px-1">
         <ProductChart chartData={productChartData.chartData} />
-        <ProductChart chartData={productChartData.chartData} />
+        <Schedule todaysSchedule={schedule.todaysSchedule}/>
       </div>
     </div>
   );
