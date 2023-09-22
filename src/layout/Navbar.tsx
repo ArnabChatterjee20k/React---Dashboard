b1c93ebeaad9fb8f3be7812238d36f0e99685149
arnab <arnabchatterjee.ac.2@gmail.com>
2023-09-22 22:26:23 +0530

    feat -- mobile menu

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,14 +1,18 @@
 import Avatar from "../components/Avatar";
+import MobileMenu from "../components/MobileMenu";
 import SearchBox from "../components/SearchBox";
 import { BiBell } from "react-icons/bi";
 export default function Navbar() {
   return (
     <nav className="flex w-full justify-between items-center">
-      <h3 className="font-monteserat font-bold text-2xl">Dashboard</h3>
-      <div className="flex items-center gap-2 md:gap-6">
+      <h3 className="font-monteserat font-bold text-xl md:text-2xl">
+        Dashboard
+      </h3>
+      <div className="flex items-center gap-5 md:gap-6">
         <SearchBox />
         <BiBell className="text-2xl" />
-        <Avatar/>
+        <Avatar />
+        <MobileMenu />
       </div>
     </nav>
   );
